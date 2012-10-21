ae134160f75aa8f702a4ca09ce39e91b10193ffb
José A. Fernández <aullasistemas@gmail.com>
2012-10-22 01:40:47 +0200

    Script para generar paquete distribuible

---
+++ build_all.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+clear
+echo -e "\nUtilidad de empaquetado de librería Eneboo Reports y dependencias"
+echo -e "(C) 2012 José A. Fernández Fernández <aullasistemas@gmail.com>\n"
+
+
+echo "* Copiando .jar necesarios para compilar en ./temp/"
+mkdir -p temp
+cp lib/jasperreports* temp
+echo "* Copiando ficheros .java en ./temp/"
+cp fuentes/*.java temp
+
+echo "* Descomprimiendo .jar de ./temp/"
+cd temp
+jar xf jasperreports-4.7.1.jar
+jar xf jasperreports-applet-4.7.1.jar
+jar xf jasperreports-fonts-4.7.1.jar
+jar xf jasperreports-javaflow-4.7.1.jar
+echo "* Compilando enebooreports.java"
+javac enebooreports.java >> /dev/null
+echo "* Compilando Xpm.java"
+javac -Xlint:unchecked Xpm.java >> /dev/null
+echo "* Generando manifiesto actualizado"
+cd ../lib
+PRIMERO="0"
+for ficheros in *.jar
+  do
+  if [ $PRIMERO == "0" ]
+  	then
+  	echo "Main-Class: enebooreports" >> ../manifiesto.txt
+    	echo "Class-Path: lib/$ficheros" >> ../manifiesto.txt
+    	PRIMERO="1"
+    	else
+    	echo "  lib/$ficheros" >> ../manifiesto.txt
+    	fi
+  done
+
+echo "* Generando enebooreports.jar"
+cd ..
+cp temp/*.class .
+jar -cmvf manifiesto.txt enebooreports.jar enebooreports.class Xpm.class otros/* >>/dev/null
+echo "* Generando EnebooReports.zip"
+zip EnebooReports.zip lib/* fonts/* enebooreports.jar >> /dev/null
+echo "* Limpiando"
+rm -fr temp
+rm -f *.class
+rm -f *.jar
+rm -f *.txt
+echo "¡¡ PROCESO TERMINADO !!"
