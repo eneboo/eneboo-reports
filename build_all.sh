27873135d7d4520c490056b14dffb1951de4ed39
José A. Fernández <aullasistemas@gmail.com>
2012-10-31 22:54:38 +0100

    SCRIPT: El script de compilación es más versátil

--- build_all.sh
+++ build_all.sh
@@ -4,25 +4,32 @@ echo -e "\nUtilidad de empaquetado de librería Eneboo Reports y dependencias"
 echo -e "(C) 2012 José A. Fernández Fernández <aullasistemas@gmail.com>\n"
 
 
-echo "* Copiando .jar necesarios para compilar en ./temp/"
+echo -e "\n* Copiando .jar necesarios para compilar en ./temp/"
 mkdir -p temp
 cp lib/jasperreports* temp
 cp lib/postgresql* temp
-echo "* Copiando ficheros .java en ./temp/"
+
+echo -e "\n* Descomprimiendo .jar de ./temp/"
+cd temp
+for ficheros in *.jar
+  do
+  echo "- $ficheros"
+  jar xf $ficheros
+  done
+
+cd ..
+echo -e "\n* Copiando ficheros .java en ./temp/"
 cp fuentes/*.java temp
 
-echo "* Descomprimiendo .jar de ./temp/"
+echo -e "\n* Compilando ... "
 cd temp
-jar xf jasperreports-4.7.1.jar
-jar xf jasperreports-applet-4.7.1.jar
-jar xf jasperreports-fonts-4.7.1.jar
-jar xf jasperreports-javaflow-4.7.1.jar
-jar xf postgresql-9.0-801.jdbc4.jar
-echo "* Compilando enebooreports.java"
-javac enebooreports.java >> /dev/null
-echo "* Compilando Xpm.java"
-javac -Xlint:unchecked Xpm.java >> /dev/null
-echo "* Generando manifiesto actualizado"
+for ficheros in *.java
+  do
+  echo "- $ficheros"
+  javac $ficheros >> /dev/null
+  done
+
+echo -e "\n* Generando manifiesto actualizado"
 cd ../lib
 PRIMERO="0"
 for ficheros in *.jar
@@ -37,13 +44,13 @@ for ficheros in *.jar
     	fi
   done
 
-echo "* Generando enebooreports.jar"
+echo -e "\n* Generando enebooreports.jar"
 cd ..
 cp temp/*.class .
 jar -cmvf manifiesto.txt enebooreports.jar enebooreports.class Xpm.class otros/* >>/dev/null
-echo "* Generando EnebooReports.zip"
+echo -e "\n* Generando EnebooReports.zip"
 zip EnebooReports.zip lib/* enebooreports.jar >> /dev/null
-echo "* Limpiando"
+echo -e "\n* Limpiando"
 rm -fr temp
 rm -f *.class
 rm -f *.jar
