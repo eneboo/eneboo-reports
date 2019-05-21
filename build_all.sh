0ceb2426c8457da2755fea1e5bedc468923dfb6f
José A. Fernández <aullasistemas@gmail.com>
2019-05-21 08:53:20 +0200

    pequeño ajuste compilación

--- build_all.sh
+++ build_all.sh
@@ -46,7 +46,7 @@ cd temp
 for ficheros in *.java
   do
   echo "- $ficheros"
-  javac -Xlint:deprecation -classpath ../lib:../temp $ficheros >> /dev/null
+  javac -Xlint:deprecation -proc:none -Xlint:unchecked -classpath ../lib:../temp $ficheros >> /dev/null
   done
   
 echo -e "\n* Sustituyendo clases"
